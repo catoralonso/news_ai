23d58751c217aa848954bcfc92dfde67fc661dfd
catoralonso <157717738+catoralonso@users.noreply.github.com>
2026-03-18 19:42:53 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -18,7 +18,7 @@
 # ─────────────────────────────────────────────────────────────────────────────
 
 terraform {
-  required_version = ">= 1.7"
+  required_version = ">= 1.5"
 
   required_providers {
     google = {
